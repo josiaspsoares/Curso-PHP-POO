67db2644cb65390826cf194be60267c9c8002716
josiassoares <61014395+josiassoares@users.noreply.github.com>
2020-12-05 13:22:38 -0300

    Commit on Master

---
+++ ola-mundo/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF--8">
+        <title>Olá Mundo</title>
+    </head>
+    <body>
+        <?php
+            echo "Olá, Mundo!";
+        ?> 
+    </body>
+</html>
